2d6c082f75e3d2300020d4d636774901e0c7af4a
FelipeFreitasDev <135486262+FelipeFreitasDev@users.noreply.github.com>
2023-07-25 01:57:28 -0300

    Project Created

---
+++ src/main/java/com/projetojava/amostra1/Amostra1Application.java
@@ -0,0 +1,13 @@
+package com.projetojava.amostra1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amostra1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amostra1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetojava/amostra1/Amostra1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetojava.amostra1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amostra1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
